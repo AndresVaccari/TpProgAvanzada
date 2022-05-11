110d6fd7ca371a2c6d5377521060a5858702bd9f
AndresVaccari <andresvaccari34@gmail.com>
2022-05-11 18:35:34 -0300

    Agregado mensaje error usuario

--- src/main/java/servletLogin/ServletLogin.java
+++ src/main/java/servletLogin/ServletLogin.java
@@ -1,11 +1,11 @@
 package servletLogin;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -21,14 +21,12 @@ public class ServletLogin extends HttpServlet {
      */
     public ServletLogin() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		String br = request.getParameter("br");
 		if (br != null) {
 			response.sendRedirect("registro.jsp");
@@ -44,15 +42,16 @@ public class ServletLogin extends HttpServlet {
 			Statement st = conn.createStatement();
 			
 			ResultSet rs = st.executeQuery("SELECT COUNT(*) FROM usuario WHERE usuario = '" + user + "' AND password = '" + pass + "'");
-			
-			//System.out.println(rs.getInt(1));
-			
 			rs.next();
 			
 			if (rs.getInt(1) == 1) {
 				response.sendRedirect("inicio.jsp");
 			} else {
-				response.sendRedirect("index.jsp");
+				PrintWriter out = response.getWriter();  
+				out.println("<script type=\"text/javascript\">");
+				out.println("location='index.jsp';");
+				out.println("alert('Usuario o contrasenia incorrecta');");
+				out.println("</script>");		
 			}
 			
 		} catch (Exception e) {
@@ -67,7 +66,6 @@ public class ServletLogin extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
