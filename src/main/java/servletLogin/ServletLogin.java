1f27c6853c226dea4fc20f43ad190ed97bbb033c
AndresVaccari <andresvaccari34@gmail.com>
2022-05-11 12:29:43 -0300

    Creado servletLogin

---
+++ src/main/java/servletLogin/ServletLogin.java
@@ -0,0 +1,68 @@
+package servletLogin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			String user = request.getParameter("user");
+			String pass = request.getParameter("pass");
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tpfinal", "root", "");
+			
+			Statement st = conn.createStatement();
+			
+			ResultSet rs = st.executeQuery("SELECT * FROM usuario WHERE usuario = '" + user + "' AND password = '" + pass + "'");
+			
+			System.out.println(rs.getString(1));
+			
+			if (rs != null) {
+				response.sendRedirect("inicio.jsp");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String br = request.getParameter("br");
+		if (br != null) {
+			response.sendRedirect("registro.jsp");
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
