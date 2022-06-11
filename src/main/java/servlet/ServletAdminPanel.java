f386ce2b6264d0d718418475fcff583c6bcede59
AndresVaccari <andresvaccari34@gmail.com>
2022-06-11 18:20:38 -0300

    Optimizacion codigo

--- src/main/java/servlet/ServletAdminPanel.java
+++ src/main/java/servlet/ServletAdminPanel.java
@@ -11,6 +11,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import entities.PanelAdmin;
+
 /**
  * Servlet implementation class ServletAdminPanel
  */
@@ -39,211 +41,27 @@ public class ServletAdminPanel extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			PanelAdmin panel = new PanelAdmin();
+			
 			java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tpfinal", "root", "");
 			
 			String botonDesactivar = request.getParameter("botonDesactivar");
 			if (botonDesactivar != null) {
 				Statement st = conn.createStatement();
 				st.executeUpdate("UPDATE usuario SET Estado = 2 WHERE Usuario = '" + botonDesactivar + "'");
-				Statement stContadorPendientes = conn.createStatement();
-				Statement stPendientes = conn.createStatement();
-				ResultSet contadorPentientes = stContadorPendientes.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 0" );
-				ResultSet usuariosPendientes = stPendientes.executeQuery("SELECT usuario FROM usuario WHERE Estado = 0" );
-				
-				Statement stContadorActivos = conn.createStatement();
-				Statement stActivos = conn.createStatement();
-				ResultSet contadorActivos = stContadorActivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 1" );
-				ResultSet usuariosActivos = stActivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 1" );
-				
-				Statement stContadorInactivos = conn.createStatement();
-				Statement stInactivos = conn.createStatement();
-				ResultSet contadorInactivos = stContadorInactivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 2" );		
-				ResultSet usuariosInactivos = stInactivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 2" );		
-				
-				contadorPentientes.next();
-				int pendientes = contadorPentientes.getInt(1);	
-				
-				contadorActivos.next();
-				int activos = contadorActivos.getInt(1);
-
-				contadorInactivos.next();
-				int inactivos = contadorInactivos.getInt(1);
-
-				String [] usuariosPendientesLista = new String[pendientes];
-				
-				usuariosPendientes.next();
-				for (int i = 0; i < usuariosPendientesLista.length ; i++) {
-					usuariosPendientesLista[i] = usuariosPendientes.getString(1);
-					usuariosPendientes.next();
-				}
-				
-				String [] usuariosActivosLista = new String[activos];
-				
-				usuariosActivos.next();
-				for (int i = 0; i < usuariosActivosLista.length ; i++) {
-					usuariosActivosLista[i] = usuariosActivos.getString(1);
-					usuariosActivos.next();
-				}
-				
-				String [] usuariosInactivosLista;
-				boolean hayInactivos;
-				
-				if (inactivos != 0) {
-					usuariosInactivosLista = new String[inactivos];
-					usuariosInactivos.next();
-					for (int i = 0; i < usuariosInactivosLista.length ; i++) {
-						usuariosInactivosLista[i] = usuariosInactivos.getString(1);
-						usuariosInactivos.next();
-					}
-					hayInactivos = true;
-					request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				} else {
-					usuariosInactivosLista = new String[1];
-					usuariosInactivosLista[0] = "No hay usuarios inactivos";
-					hayInactivos = false;
-				}
-				
-				request.setAttribute("usuariosActivos", usuariosActivosLista);
-				request.setAttribute("usuariosPendientes", usuariosPendientesLista);
-				request.setAttribute("hayInactivos", hayInactivos);
-				request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				request.getRequestDispatcher("adminpanel.jsp").forward(request, response);	
+				panel.actualizarPanel(request, response);
 			}
 			String botonActivar = request.getParameter("botonActivar");
 			if (botonActivar != null) {
 				Statement st = conn.createStatement();
 				st.executeUpdate("UPDATE usuario SET Estado = 1 WHERE Usuario = '" + botonActivar + "'");
-				Statement stContadorPendientes = conn.createStatement();
-				Statement stPendientes = conn.createStatement();
-				ResultSet contadorPentientes = stContadorPendientes.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 0" );
-				ResultSet usuariosPendientes = stPendientes.executeQuery("SELECT usuario FROM usuario WHERE Estado = 0" );
-				
-				Statement stContadorActivos = conn.createStatement();
-				Statement stActivos = conn.createStatement();
-				ResultSet contadorActivos = stContadorActivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 1" );
-				ResultSet usuariosActivos = stActivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 1" );
-				
-				Statement stContadorInactivos = conn.createStatement();
-				Statement stInactivos = conn.createStatement();
-				ResultSet contadorInactivos = stContadorInactivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 2" );		
-				ResultSet usuariosInactivos = stInactivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 2" );		
-				
-				contadorPentientes.next();
-				int pendientes = contadorPentientes.getInt(1);	
-				
-				contadorActivos.next();
-				int activos = contadorActivos.getInt(1);
-
-				contadorInactivos.next();
-				int inactivos = contadorInactivos.getInt(1);
-
-				String [] usuariosPendientesLista = new String[pendientes];
-				
-				usuariosPendientes.next();
-				for (int i = 0; i < usuariosPendientesLista.length ; i++) {
-					usuariosPendientesLista[i] = usuariosPendientes.getString(1);
-					usuariosPendientes.next();
-				}
-				
-				String [] usuariosActivosLista = new String[activos];
-				
-				usuariosActivos.next();
-				for (int i = 0; i < usuariosActivosLista.length ; i++) {
-					usuariosActivosLista[i] = usuariosActivos.getString(1);
-					usuariosActivos.next();
-				}
-				
-				String [] usuariosInactivosLista;
-				boolean hayInactivos;
-				
-				if (inactivos != 0) {
-					usuariosInactivosLista = new String[inactivos];
-					usuariosInactivos.next();
-					for (int i = 0; i < usuariosInactivosLista.length ; i++) {
-						usuariosInactivosLista[i] = usuariosInactivos.getString(1);
-						usuariosInactivos.next();
-					}
-					hayInactivos = true;
-					request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				} else {
-					usuariosInactivosLista = new String[1];
-					usuariosInactivosLista[0] = "No hay usuarios inactivos";
-					hayInactivos = false;
-				}
-				
-				request.setAttribute("usuariosActivos", usuariosActivosLista);
-				request.setAttribute("usuariosPendientes", usuariosPendientesLista);
-				request.setAttribute("hayInactivos", hayInactivos);
-				request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				request.getRequestDispatcher("adminpanel.jsp").forward(request, response);	
+				panel.actualizarPanel(request, response);
 			}
 			String botonRechazar = request.getParameter("botonRechazar");;
 			if (botonRechazar != null) {
 				Statement st = conn.createStatement();
 				st.executeUpdate("DELETE FROM usuario WHERE Usuario = '" + botonRechazar + "'");
-				Statement stContadorPendientes = conn.createStatement();
-				Statement stPendientes = conn.createStatement();
-				ResultSet contadorPentientes = stContadorPendientes.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 0" );
-				ResultSet usuariosPendientes = stPendientes.executeQuery("SELECT usuario FROM usuario WHERE Estado = 0" );
-				
-				Statement stContadorActivos = conn.createStatement();
-				Statement stActivos = conn.createStatement();
-				ResultSet contadorActivos = stContadorActivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 1" );
-				ResultSet usuariosActivos = stActivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 1" );
-				
-				Statement stContadorInactivos = conn.createStatement();
-				Statement stInactivos = conn.createStatement();
-				ResultSet contadorInactivos = stContadorInactivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 2" );		
-				ResultSet usuariosInactivos = stInactivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 2" );		
-				
-				contadorPentientes.next();
-				int pendientes = contadorPentientes.getInt(1);	
-				
-				contadorActivos.next();
-				int activos = contadorActivos.getInt(1);
-
-				contadorInactivos.next();
-				int inactivos = contadorInactivos.getInt(1);
-
-				String [] usuariosPendientesLista = new String[pendientes];
-				
-				usuariosPendientes.next();
-				for (int i = 0; i < usuariosPendientesLista.length ; i++) {
-					usuariosPendientesLista[i] = usuariosPendientes.getString(1);
-					usuariosPendientes.next();
-				}
-				
-				String [] usuariosActivosLista = new String[activos];
-				
-				usuariosActivos.next();
-				for (int i = 0; i < usuariosActivosLista.length ; i++) {
-					usuariosActivosLista[i] = usuariosActivos.getString(1);
-					usuariosActivos.next();
-				}
-				
-				String [] usuariosInactivosLista;
-				boolean hayInactivos;
-				
-				if (inactivos != 0) {
-					usuariosInactivosLista = new String[inactivos];
-					usuariosInactivos.next();
-					for (int i = 0; i < usuariosInactivosLista.length ; i++) {
-						usuariosInactivosLista[i] = usuariosInactivos.getString(1);
-						usuariosInactivos.next();
-					}
-					hayInactivos = true;
-					request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				} else {
-					usuariosInactivosLista = new String[1];
-					usuariosInactivosLista[0] = "No hay usuarios inactivos";
-					hayInactivos = false;
-				}
-				
-				request.setAttribute("usuariosActivos", usuariosActivosLista);
-				request.setAttribute("usuariosPendientes", usuariosPendientesLista);
-				request.setAttribute("hayInactivos", hayInactivos);
-				request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				request.getRequestDispatcher("adminpanel.jsp").forward(request, response);	
+				panel.actualizarPanel(request, response);
 			}
 				
 		} catch (Exception e) {
--- src/main/java/servlet/ServletInicio.java
+++ src/main/java/servlet/ServletInicio.java
@@ -9,6 +9,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import entities.PanelAdmin;
+
 /**
  * Servlet implementation class ServletInicio
  */
@@ -55,70 +57,8 @@ public class ServletInicio extends HttpServlet {
 			}
 			String botonAdmin = request.getParameter("botonAdmin");
 			if (botonAdmin != null) {
-				Statement stContadorPendientes = conn.createStatement();
-				Statement stPendientes = conn.createStatement();
-				ResultSet contadorPentientes = stContadorPendientes.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 0" );
-				ResultSet usuariosPendientes = stPendientes.executeQuery("SELECT usuario FROM usuario WHERE Estado = 0" );
-				
-				Statement stContadorActivos = conn.createStatement();
-				Statement stActivos = conn.createStatement();
-				ResultSet contadorActivos = stContadorActivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 1" );
-				ResultSet usuariosActivos = stActivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 1" );
-				
-				Statement stContadorInactivos = conn.createStatement();
-				Statement stInactivos = conn.createStatement();
-				ResultSet contadorInactivos = stContadorInactivos.executeQuery("SELECT COUNT(*) FROM usuario WHERE Estado = 2" );		
-				ResultSet usuariosInactivos = stInactivos.executeQuery("SELECT usuario FROM usuario WHERE Estado = 2" );		
-				
-				contadorPentientes.next();
-				int pendientes = contadorPentientes.getInt(1);	
-				
-				contadorActivos.next();
-				int activos = contadorActivos.getInt(1);
-
-				contadorInactivos.next();
-				int inactivos = contadorInactivos.getInt(1);
-
-				String [] usuariosPendientesLista = new String[pendientes];
-				
-				usuariosPendientes.next();
-				for (int i = 0; i < usuariosPendientesLista.length ; i++) {
-					usuariosPendientesLista[i] = usuariosPendientes.getString(1);
-					usuariosPendientes.next();
-				}
-				
-				String [] usuariosActivosLista = new String[activos];
-				
-				usuariosActivos.next();
-				for (int i = 0; i < usuariosActivosLista.length ; i++) {
-					usuariosActivosLista[i] = usuariosActivos.getString(1);
-					usuariosActivos.next();
-				}
-				
-				String [] usuariosInactivosLista;
-				boolean hayInactivos;
-				
-				if (inactivos != 0) {
-					usuariosInactivosLista = new String[inactivos];
-					usuariosInactivos.next();
-					for (int i = 0; i < usuariosInactivosLista.length ; i++) {
-						usuariosInactivosLista[i] = usuariosInactivos.getString(1);
-						usuariosInactivos.next();
-					}
-					hayInactivos = true;
-					request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				} else {
-					usuariosInactivosLista = new String[1];
-					usuariosInactivosLista[0] = "No hay usuarios inactivos";
-					hayInactivos = false;
-				}
-				
-				request.setAttribute("usuariosActivos", usuariosActivosLista);
-				request.setAttribute("usuariosPendientes", usuariosPendientesLista);
-				request.setAttribute("hayInactivos", hayInactivos);
-				request.setAttribute("usuariosInactivos", usuariosInactivosLista);
-				request.getRequestDispatcher("adminpanel.jsp").forward(request, response);		
-				
+				PanelAdmin panel = new PanelAdmin();
+				panel.actualizarPanel(request, response);
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
